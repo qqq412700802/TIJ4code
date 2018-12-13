0665960e1e2de145f53b5dc063dba59dc4511251
lixj <412700802@qq.com>
2018-12-14 00:54:14 +0800

    NIO

--- src/main/java/com/lixj/io/nio/TransferTo.java
+++ src/main/java/com/lixj/io/nio/TransferTo.java
@@ -1,7 +1,19 @@
 package com.lixj.io.nio;
 
-public class TransferTo {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
+public class TransferTo {
+    public static void main(String[] args) throws IOException {
+        FileChannel inFileChannel = new FileInputStream("/柳园下.txt").getChannel();
+
+        System.out.println(inFileChannel.size());
+
+        FileChannel outFileChannel = new FileOutputStream("/out.txt").getChannel();
+
+        inFileChannel.transferTo(0, inFileChannel.size(), outFileChannel);
     }
 }
